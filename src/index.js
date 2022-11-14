567e3413a0143ea6b96037a9583814eb235a3d72
Robson <41270745+ROSS1996@users.noreply.github.com>
2022-11-14 18:59:06 -0300

    Implement search bar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import './style.css'
 
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+let city = urlParams.get('city')
+
+if (city === null) {
+    city = 'Rio de Janeiro'
+}
+
+const searchDOM = document.getElementById('searchCity')
+
 const cityNameDOM = document.getElementById('cityName')
 const weatherDOM = document.getElementById('weather')
 const temperatureDOM = document.getElementById('temperature')
@@ -10,20 +21,23 @@ const humidityDOM = document.getElementById('humidity')
 
 
 async function getData (city) {
+    let weatherInfo;
     try {
-        const weatherInfo = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=4ac58ea560a4d5f7c1bffa9834a17203`)
-        const weatherObject = weatherInfo.json()
-        return weatherObject
+        weatherInfo = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=4ac58ea560a4d5f7c1bffa9834a17203`)
+        if (!weatherInfo.ok) {
+            alert('City not found')
+        } else {
+            const weatherObject = weatherInfo.json()
+            return weatherObject
+        }
     } catch (err) {
         alert(err)
     }
 
 }
 
-let teste = getData('Rio de Janeiro');
-
-teste.then( function (x) {
-    console.log(x)
+const info = getData(city)
+info.then( function (x) {
     cityNameDOM.innerText = `${x.name}, ${x.sys.country}`,
     weatherDOM.innerText = `${x.weather[0].main}`,
     temperatureDOM.innerText = `${x.main.temp}º`,
